44df07b7dc464060ca750538b3f2adc55f2f58eb
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-07-28 20:52:10 -0300

    Configurando o Comando

--- src/App/Command/CreateUserCommand.php
+++ src/App/Command/CreateUserCommand.php
@@ -11,9 +11,24 @@ class CreateUserCommand extends Command
     // o nome do comando
     protected static $defaultName = 'app:create-user';
 
+    public function __construct(bool $requirePassword = false)
+    {
+        // as melhores práticas recomendam chamar o construtor pai primeiro e, em seguida,
+        // definir suas próprias propriedades. Isso não funcionaria neste caso porque
+        // configure() precisa das propriedades definidas neste construtor
+        $this->requirePassword = $requirePassword;
+
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
-        // ...
+        $this
+        // a breve descrição mostrada durante a execução do comando
+        ->setDescription('Creates a new user.')
+
+        // a descrição completa do comando mostrada ao executar o comando com a opção "--help"
+            ->setHelp('This command allows you to create a user...');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
