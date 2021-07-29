3e762212909d1ed4f595c4dae4270d2d4d68cdf7
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-07-28 21:16:54 -0300

    Seções de saída

--- app.php
+++ app.php
@@ -3,11 +3,13 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use App\Command\CreateUserCommand;
+use App\Command\MyCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 
 // ... register commands
 $application->add(new CreateUserCommand());
+$application->add(new MyCommand());
 
 $application->run();
---
+++ src/App/Command/MyCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MyCommand extends Command
+{
+    protected static $defaultName = 'app:my-user';
+
+    protected function configure(): void
+    {
+        $this->setDescription('Comando test.')
+            ->setHelp('This command allows ...');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        if (!$output instanceof ConsoleOutputInterface) {
+            throw new \LogicException('This command accepts only an instance of "ConsoleOutputInterface".');
+        }
+
+        $section1 = $output->section();
+        $section2 = $output->section();
+
+        $section1->writeln('Hello');
+        $section2->writeln('World!');
+        // Output displays "Hello\nWorld!\n"
+
+        // overwrite() substitui todo o conteúdo da seção existente pelo conteúdo fornecido
+        $section1->overwrite('Goodbye');
+        // Output now displays "Goodbye\nWorld!\n"
+
+        // clear() apaga todo o conteúdo da seção...
+        $section2->clear();
+        // Output now displays "Goodbye\n"
+
+        // ...mas você também pode deletar um determinado número de linhas
+        // (este exemplo exclui as duas últimas linhas da seção)
+        $section1->clear(2);
+        // Output is now completely empty!
+
+        return Command::SUCCESS;
+    }
+}
