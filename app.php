2bb9b00fba713963c5644e2e19586f85492ea796
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-07-28 21:36:44 -0300

    Config

--- app.php
+++ app.php
@@ -2,16 +2,14 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use App\Command\CreateUserCommand;
-use App\Command\GreetCommand;
-use App\Command\MyCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 
-// ... register commands
-$application->add(new CreateUserCommand());
-$application->add(new MyCommand());
-$application->add(new GreetCommand());
+$commands = require __DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'commands.php';
+
+foreach ($commands as $commandName) {
+    $application->add(new $commandName);
+}
 
 $application->run();
---
+++ config/commands.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Command\CreateUserCommand;
+use App\Command\GreetCommand;
+use App\Command\MyCommand;
+
+return [
+    CreateUserCommand::class,
+    MyCommand::class,
+    GreetCommand::class,
+];
