f3c3f1ee65caf105188c08c248d326c1b1ae2838
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-07-28 21:01:31 -0300

    Registrar o comando

--- app.php
+++ app.php
@@ -2,10 +2,12 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use App\Command\CreateUserCommand;
 use Symfony\Component\Console\Application;
 
 $application = new Application();
 
 // ... register commands
+$application->add(new CreateUserCommand());
 
 $application->run();
